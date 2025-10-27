4b12109041de97016948f0914aa3be9635e76e49
rai-watanabe <rai0w9a1t0anabe@icloud.com>
2025-10-27 10:57:56 -0700

    feat: create contact page

---
+++ src/pages/Contact.tsx
@@ -0,0 +1,21 @@
+export function Contact() {
+  return (
+    <section className="max-w-3xl">
+      <h1 className="text-3xl font-bold text-main mb-4">Contact</h1>
+      <p className="text-light/80">Feel free to contact me!</p>
+      <ul className="list-disc list-inside">
+        <li>
+          <a href="mailto:rai.watanabe910@gmail.com">
+            rai.watanabe910@gmail.com
+          </a>
+        </li>
+        <li>
+          <a href="https://github.com/rai-wtnb">GitHub</a>
+        </li>
+        <li>
+          <a href="https://linkedin.com/in/rai-wtnb">LinkedIn</a>
+        </li>
+      </ul>
+    </section>
+  );
+}
